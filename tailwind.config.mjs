d2337d9b791157761ccc57f0ea5534c325ce510d
nicodemus <nicholaspeloeahae@gmail.com>
2024-02-15 15:30:14 +0200

    added images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			height: {
+				'85v': '85vh',
+				'100v': '100vh'
+
+			},
+			backgroundImage:{
+				'index-hero': "url('/src/assets/vienna-kota-bg.jpg')"
+			}
+		},
 	},
 	plugins: [],
 }
